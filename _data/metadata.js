05936227d66797241582377bf1c9d75a57f638dd
Alain Coletta <alaincoletta@gmail.com>
2024-05-31 14:11:57 +0200

    changed metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,9 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Genes Of Thrones Blog	",
+	url: "https://genesofthrones.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Welcome to Genes of Thrones, an epic saga where ancient power struggles meet cutting-edge genetics. Dive into the captivating world of Genoria, a kingdom where the fate of noble houses is determined not only by their lineage but by their unique genetic traits.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Dr Mendel",
 	}
 }
